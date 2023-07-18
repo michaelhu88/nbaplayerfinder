41bf42162bd5cc4c274cb88ff8ed7f90a0fac124
Michael Hu <michaelhu88@gmail.com>
2023-07-17 21:23:36 -0700

    "Interacted with url parameters"

--- nbaplayerfinder.py
+++ nbaplayerfinder.py
@@ -2,11 +2,7 @@ from flask import Flask
 import requests
 import pprint
 
-url = "https://www.balldontlie.io/api/v1/players"
-
-get_players = requests.get(url)
-get_players_json = get_players.json()
-pprint.pprint(get_players_json)
+url = "https://www.balldontlie.io/api/v1/players?page=1&per_page=10"
 
 
 
@@ -17,6 +13,7 @@ def home():
     get_players = requests.get(url)
     get_players_json = get_players.json()
     pprint.pprint(get_players_json)
+    return "NBA Players"
 
 if __name__ == "__main__":
     app.run()
