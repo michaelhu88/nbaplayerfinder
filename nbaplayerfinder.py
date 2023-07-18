6f30b636ceb08806664dd12fabd6bf223779a7c7
Michael Hu <michaelhu88@gmail.com>
2023-07-17 22:26:04 -0700

    "Templates added"

--- nbaplayerfinder.py
+++ nbaplayerfinder.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, redirect, request, url_for, render_template
 import requests
 import pprint
+
 
 url = "https://www.balldontlie.io/api/v1/players?page=1&per_page=10"
 
@@ -8,12 +9,16 @@ url = "https://www.balldontlie.io/api/v1/players?page=1&per_page=10"
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ["POST", "GET"])
 def home():
-    get_players = requests.get(url)
-    get_players_json = get_players.json()
-    pprint.pprint(get_players_json)
-    return "NBA Players"
+    #get_players = requests.get(url)
+    #get_players_json = get_players.json()
+    #pprint.pprint(get_players_json)
+    return render_template("input.html")
 
 if __name__ == "__main__":
     app.run()
+
+#what do i have to do? 
+# the end user inputs a NBA Player, i take that information and return the player's stats
+
